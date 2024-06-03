49726317629db39196934b9e86e0c9186960f39b
Rosen Marinov <rosen1710@abv.bg>
2024-06-03 17:40:19 +0300

    Simple table added

--- src/test/java/com/example/api/ApiApplicationTests.java
+++ src/test/java/com/example/api/ApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,9 +9,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class ApiApplicationTests {
+	@Getter
+	@AllArgsConstructor
+	private static class Car {
+		String make;
+		String model;
+
+		public static void insert(Connection connection, Car car) {
+			if (connection == null) {
+				throw new RuntimeException("Your connection can't be null!");
+			}
+            try {
+                connection.prepareStatement("insert into car (make, model) values ('%s', '%s');".formatted(car.make, car.model)).executeUpdate();
+            }
+			catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+		public static List<Car> parseFromResultSet(ResultSet resultSet) {
+			List<Car> cars = new ArrayList<>();
+			try {
+				while (resultSet.next()) {
+					cars.add(new Car(resultSet.getString("make"), resultSet.getString("model")));
+				}
+			}
+			catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return cars;
+		}
+	}
 
 	@Test
 	void contextLoads() {
@@ -22,16 +57,20 @@ class ApiApplicationTests {
 
 		try {
 			Connection connection = dataSource.getConnection();
-			ResultSet resultSet = connection.prepareStatement("select * from city").executeQuery();
-			while (resultSet.next()) {
-				System.out.println(resultSet.getBigDecimal("id"));
-				System.out.println(resultSet.getString("name"));
-				System.out.println(resultSet.getString("timezone"));
-				System.out.println(resultSet.getString("country_iso2country_code"));
+
+			connection.prepareStatement("create table if not exists car (id serial primary key, make text not null, model text not null);").executeUpdate();
+
+			Car.insert(connection, new Car("VW", "Golf"));
+
+			ResultSet resultSet = connection.prepareStatement("select * from car;").executeQuery();
+            for (Car car : Car.parseFromResultSet(resultSet)) {
+				System.out.printf("%s %s%n", car.getMake(), car.getModel());
 			}
-		} catch (SQLException e) {
+
+			connection.close();
+		}
+		catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
