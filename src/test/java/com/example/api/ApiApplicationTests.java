5793068c3941612c21d5d640a50c9cc76d44d5b5
Rosen Marinov <rosen1710@abv.bg>
2024-06-06 19:11:58 +0300

    Added engine entity

--- src/test/java/com/example/api/ApiApplicationTests.java
+++ src/test/java/com/example/api/ApiApplicationTests.java
@@ -7,6 +7,7 @@ import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,27 +17,87 @@ import java.util.List;
 class ApiApplicationTests {
 	@Getter
 	@AllArgsConstructor
+	private static class Engine {
+		private int id;
+		private String engineCode;
+		private String make;
+
+		public static void insert(Connection connection, Engine engine) {
+			if (connection == null) {
+				throw new RuntimeException("Your connection can't be null!");
+			}
+			try {
+				PreparedStatement statement = connection.prepareStatement("insert into engine (id, engine_code, make) values (?, ?, ?);");
+				statement.setLong(1, engine.id);
+				statement.setString(2, engine.engineCode);
+				statement.setString(3, engine.make);
+				statement.executeUpdate();
+			}
+			catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		public static List<Engine> parseFromResultSet(ResultSet resultSet) {
+			List<Engine> engines = new ArrayList<>();
+			try {
+				while (resultSet.next()) {
+					engines.add(new Engine(resultSet.getInt("id"), resultSet.getString("engine_code"), resultSet.getString("make")));
+				}
+			}
+			catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+			return engines;
+		}
+
+		public static Engine getById(Connection connection, int id) {
+			if (connection == null) {
+				throw new RuntimeException("Your connection can't be null!");
+			}
+			try {
+				PreparedStatement statement = connection.prepareStatement("select * from engine where id = ?;");
+				statement.setLong(1, id);
+				return Engine.parseFromResultSet(statement.executeQuery()).getFirst();
+			}
+			catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	@Getter
+	@AllArgsConstructor
 	private static class Car {
-		String make;
-		String model;
+		private int id;
+		private String make;
+		private String model;
+		private Engine engine;
 
 		public static void insert(Connection connection, Car car) {
 			if (connection == null) {
 				throw new RuntimeException("Your connection can't be null!");
 			}
             try {
-                connection.prepareStatement("insert into car (make, model) values ('%s', '%s');".formatted(car.make, car.model)).executeUpdate();
+                PreparedStatement statement = connection.prepareStatement("insert into car (id, make, model, engine_id) values (?, ?, ?, ?);");
+				statement.setLong(1, car.id);
+				statement.setString(2, car.make);
+				statement.setString(3, car.model);
+				statement.setInt(4, car.engine.getId());
+				statement.executeUpdate();
             }
 			catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
 
-		public static List<Car> parseFromResultSet(ResultSet resultSet) {
+		public static List<Car> parseFromResultSet(Connection connection, ResultSet resultSet) {
 			List<Car> cars = new ArrayList<>();
 			try {
 				while (resultSet.next()) {
-					cars.add(new Car(resultSet.getString("make"), resultSet.getString("model")));
+					//lazy bool parameter addition + using it for lazy load or not
+					Engine engine = Engine.getById(connection, resultSet.getInt("engine_id"));
+					cars.add(new Car(resultSet.getInt("id"), resultSet.getString("make"), resultSet.getString("model"), engine));
 				}
 			}
 			catch (SQLException e) {
@@ -58,14 +119,22 @@ class ApiApplicationTests {
 		try {
 			Connection connection = dataSource.getConnection();
 
-			connection.prepareStatement("create table if not exists car (id serial primary key, make text not null, model text not null);").executeUpdate();
+			connection.prepareStatement("create table if not exists engine (id serial primary key, engine_code text not null, make text not null);").executeUpdate();
+			connection.prepareStatement("create table if not exists car (id serial primary key, make text not null, model text not null, engine_id int references engine(id) not null);").executeUpdate();
 
-			Car.insert(connection, new Car("VW", "Golf"));
+			Engine engine = new Engine(1, "VW150hp", "VW-Engines");
+			Engine.insert(connection, engine);
+			Car.insert(connection, new Car(1, "VW", "Tiguan", engine));
 
-			ResultSet resultSet = connection.prepareStatement("select * from car;").executeQuery();
-            for (Car car : Car.parseFromResultSet(resultSet)) {
-				System.out.printf("%s %s%n", car.getMake(), car.getModel());
+			ResultSet resultSet = connection.prepareStatement("select * from car join engine on car.engine_id = engine.id;").executeQuery();
+            for (Car car : Car.parseFromResultSet(connection, resultSet)) {
+				System.out.printf("%d %s %s | %d %s %s\n", car.getId(), car.getMake(), car.getModel(), car.getEngine().getId(), car.getEngine().getEngineCode(), car.getEngine().getMake());
 			}
+
+//			resultSet = connection.prepareStatement("select * from engine;").executeQuery();
+//            for (Engine myEngine : Engine.parseFromResultSet(resultSet)) {
+//				System.out.printf("%d %s %s%n", myEngine.getId(), myEngine.getEngineCode(), myEngine.getMake());
+//			}
 
 			connection.close();
 		}
